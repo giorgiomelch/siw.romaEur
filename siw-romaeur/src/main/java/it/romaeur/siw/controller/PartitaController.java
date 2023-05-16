981bf97aa2cea2fab26bacf4126802cacb17a208
gianluca8079 <92943708+gianluca8079@users.noreply.github.com>
2023-05-17 01:15:00 +0200

    form per aggiunta partita

---
+++ siw-romaeur/src/main/java/it/romaeur/siw/controller/PartitaController.java
@@ -0,0 +1,47 @@
+package it.romaeur.siw.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import it.romaeur.siw.model.Partita;
+import it.romaeur.siw.repository.PartitaRepository;
+import jakarta.validation.Valid;
+
+@Controller
+public class PartitaController {
+	
+	@Autowired PartitaRepository partitaRepository; 
+	
+	@GetMapping("/calendario")
+	public String calendario(Model model) {
+		model.addAttribute("calendario", this.partitaRepository.findAll());
+		return "calendario.html";
+	}
+	
+	@GetMapping(value="/formNuovaPartita")
+	public String formNuovaPartita(Model model) {
+		model.addAttribute("partita", new Partita());
+		return "formNuovaPartita.html";
+	}
+	
+	@PostMapping("/partita")
+	public String newPartita( @ModelAttribute("partita") Partita partita, Model model) {
+		
+			this.partitaRepository.save(partita); 
+			model.addAttribute("partita", partita);
+			return "partita.html";
+		} 
+	
+	@GetMapping("/partita/{id}")
+	public String getPartita(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("partita", this.partitaRepository.findById(id).get());
+		return "partita.html";
+	}
+
+
+}
