fc0345ba43137f64f2a04d5d9b28292a3e8b746e
giorgiomelch <melchiorrigiorgio@gmail.com>
2023-05-19 13:31:28 +0200

    import partita

--- siw-romaeur/src/main/resources/import.sql
+++ siw-romaeur/src/main/resources/import.sql
@@ -3,3 +3,5 @@ insert into giocatore values(nextval('giocatore_seq'), 'Gianluca', '28/02/2001',
 insert into giocatore values(nextval('giocatore_seq'), 'Daniele', '28/02/2001', 'Gai', 3, 'Guardia tiratrice');
 insert into giocatore values(nextval('giocatore_seq'), 'Giulio', '28/02/2001', 'Gramegna', 11, 'Playmaker');
 insert into giocatore values(nextval('giocatore_seq'), 'Matteo', '25/05/1998', 'Melchiorri', 9, 'Ala piccola');
+
+insert into partita values(nextval('partita_seq'), '17/05/2023', 'Colleferro', 68, 63);
