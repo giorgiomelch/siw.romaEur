2f93806465f56be3a6e80552bedddf0bda22cd3f
gianluca8079 <92943708+gianluca8079@users.noreply.github.com>
2023-05-22 18:47:54 +0200

    Bio giocatore

--- siw-romaeur/src/main/resources/import.sql
+++ siw-romaeur/src/main/resources/import.sql
@@ -1,7 +1,7 @@
-insert into giocatore values(nextval('giocatore_seq'), 'Giorgio', '10/02/2001', 'Melchiorri', 31, 'Ala grande');
-insert into giocatore values(nextval('giocatore_seq'), 'Gianluca', '10/02/2001', 'Proietti', 13, 'Centro');
-insert into giocatore values(nextval('giocatore_seq'), 'Daniele', '10/02/2001', 'Gai', 3, 'Guardia tiratrice');
-insert into giocatore values(nextval('giocatore_seq'), 'Giulio', '10/02/2001', 'Gramegna', 11, 'Playmaker');
-insert into giocatore values(nextval('giocatore_seq'), 'Matteo', '10/05/1998', 'Melchiorri', 9, 'Ala piccola');
+insert into giocatore values(nextval('giocatore_seq'), 'Melchiorri', '10/02/2001', 'Giorgio', 31, 'Ala grande','/images/basketball.png');
+insert into giocatore values(nextval('giocatore_seq'), 'Proietti', '10/02/2001', 'Gianluca', 13, 'Centro','/images/basketball.png');
+insert into giocatore values(nextval('giocatore_seq'), 'Gai', '10/02/2001', 'Daniele', 3, 'Guardia tiratrice','/images/basketball.png');
+insert into giocatore values(nextval('giocatore_seq'), 'Gramegna', '10/02/2001', 'Giulio', 11, 'Playmaker','/images/basketball.png');
+insert into giocatore values(nextval('giocatore_seq'), 'Melchiorri', '10/05/1998', 'Matteo', 9, 'Ala piccola','/images/basketball.png');
 
 insert into partita values(nextval('partita_seq'), '11/05/2023', 'Colleferro', 68, 63);
