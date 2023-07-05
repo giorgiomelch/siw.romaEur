50e3fcaf88efb4fcb1d2024dfea1008fe708b05a
giorgiomelch <melchiorrigiorgio@gmail.com>
2023-07-05 16:15:15 +0200

    navigabilità in calendario

--- siw-romaeur/src/main/resources/import.sql
+++ siw-romaeur/src/main/resources/import.sql
@@ -1,10 +1,10 @@
-insert into giocatore values(nextval('giocatore_seq'), 'Melchiorri', '10/02/2001', 'Giorgio', 31, 'Ala grande','/images/Alfred.png');
-insert into giocatore values(nextval('giocatore_seq'), 'Proietti', '10/02/2001', 'Gianluca', 13, 'Centro','/images/basketball.png');
-insert into giocatore values(nextval('giocatore_seq'), 'Gai', '10/02/2001', 'Daniele', 3, 'Guardia tiratrice','/images/basketball.png');
-insert into giocatore values(nextval('giocatore_seq'), 'Gramegna', '10/02/2001', 'Giulio', 11, 'Playmaker','/images/basketball.png');
-insert into giocatore values(nextval('giocatore_seq'), 'Melchiorri', '10/05/1998', 'Matteo', 9, 'Ala piccola','/images/basketball.png');
+insert into giocatore values(nextval('giocatore_seq'), 'Melchiorri', '10/02/2001', 'Giorgio', 31, 'Ala grande');
+insert into giocatore values(nextval('giocatore_seq'), 'Proietti', '10/02/2001', 'Gianluca', 13, 'Centro');
+insert into giocatore values(nextval('giocatore_seq'), 'Gai', '10/02/2001', 'Daniele', 3, 'Guardia tiratrice');
+insert into giocatore values(nextval('giocatore_seq'), 'Gramegna', '10/02/2001', 'Giulio', 11, 'Playmaker');
+insert into giocatore values(nextval('giocatore_seq'), 'Melchiorri', '10/05/1998', 'Matteo', 9, 'Ala piccola');
 
-insert into partita values(nextval('partita_seq'), '11/05/2023', 'Colleferro', 68, 63, 'https://static.csi-net.it/modulistica/logo/00001047.jpg');
+insert into partita values(nextval('partita_seq'), '11/05/2023', 'Colleferro', 68, 63);
 
 
 
